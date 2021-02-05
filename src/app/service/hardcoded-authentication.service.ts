39cca6ac305923bfdf4b2ccf300e406e2d033b7b
Neha151089-git <neha.agrawal151089@gmail.com>
2021-02-05 17:38:33 +0000

    Initial Commit

---
+++ src/app/service/hardcoded-authentication.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { BasicAuthenticationService } from './basic-authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HardcodedAuthenticationService {
+
+  constructor(private basicAuthenticationService: BasicAuthenticationService) { }
+
+  authenticate(username, password) {
+    //console.log('before ' + this.isUserLoggedIn());
+    if(username==="neha" && password === 'dummy') {
+      sessionStorage.setItem('authenticaterUser', username);
+      //console.log('after ' + this.isUserLoggedIn());
+      return true;
+    }
+    return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticaterUser')
+    return !(user === null)
+  }
+
+  logout(){
+    sessionStorage.removeItem('authenticaterUser');
+    this.basicAuthenticationService.authenticatedUserName = '';
+  }
+
+}
